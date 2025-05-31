63ab02e717d0aff710b15b8cb364c21f180c31bc
user <161374229+r3darchi@users.noreply.github.com>
2025-05-31 19:16:56 +0400

    feat(cmd): add -file flag

--- cmd/analyzer/main.go
+++ cmd/analyzer/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	filepath := flag.String("file", "", "Path to log file (required)")
+
+	flag.Parse()
+
+	if *filepath == "" {
+		fmt.Println("Flag -file not set")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 }
