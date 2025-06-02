7514920e5d61827a510d5797e6a5f55ec194ced2
user <161374229+r3darchi@users.noreply.github.com>
2025-06-02 17:56:45 +0400

    feat(models): add app config for yaml

--- internal/models/config.go
+++ internal/models/config.go
@@ -1,9 +1,15 @@
 package models
 
-type LoggerConfig struct {
-	LogFile    string
-	LogLevel   string
-	MaxSizeMB  int
-	MaxBackups int
-	MaxAgeDays int
+type AppConfig struct {
+	LoggerConfig `yaml:"logger_config"`
 }
+
+type LoggerConfig struct {
+	LogFile    string `yaml:"log_file"`
+	LogLevel   string `yaml:"log_level"`
+	MaxSizeMB  int    `yaml:"max_size_mb"`
+	MaxBackups int    `yaml:"max_backups"`
+	MaxAgeDays int    `yaml:"max_age_days"`
+}
+
+//TODO: add validation method for app config
