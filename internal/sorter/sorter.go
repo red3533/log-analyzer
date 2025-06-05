5aa7add36f12cdaad204be26f70e99b6f3538629
user <161374229+r3darchi@users.noreply.github.com>
2025-06-05 15:04:05 +0400

    refactor(sorter): add comparators instead of long switch case

--- internal/sorter/sorter.go
+++ internal/sorter/sorter.go
@@ -14,54 +14,31 @@ type LogSorter struct {
 
 func (s LogSorter) Sort(logs []models.Log, sortField string, sortBy string) error {
 
-	switch sortField {
-	case "ip":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].IP < logs[j].IP })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].IP > logs[j].IP })
+	// TODO: add validation
 
-	case "timestamp":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].Timestamp.Before(logs[j].Timestamp) })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].Timestamp.After(logs[j].Timestamp) })
+	comparators := map[string]func(i, j models.Log) bool{
+		"ip":        func(i, j models.Log) bool { return i.IP < j.IP },
+		"timestamp": func(i, j models.Log) bool { return i.Timestamp.Before(j.Timestamp) },
+		"method":    func(i, j models.Log) bool { return i.Method < j.Method },
+		"url":       func(i, j models.Log) bool { return i.URL < j.URL },
+		"status":    func(i, j models.Log) bool { return i.Status < j.Status },
+		"size_byte": func(i, j models.Log) bool { return i.SizeByte < j.SizeByte },
+	}
 
-	case "method":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].Method < logs[j].Method })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].Method > logs[j].Method })
-
-	case "url":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].URL < logs[j].URL })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].URL > logs[j].URL })
-
-	case "status":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].Status < logs[j].Status })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].Status > logs[j].Status })
-
-	case "size_byte":
-		if sortBy == "asc" {
-			sort.SliceStable(logs, func(i, j int) bool { return logs[i].SizeByte < logs[j].SizeByte })
-			return nil
-		}
-		sort.SliceStable(logs, func(i, j int) bool { return logs[i].SizeByte > logs[j].SizeByte })
-
-	default:
+	comparator, ok := comparators[sortField]
+	if !ok {
 		return fmt.Errorf("unknown sort field: %s", sortField)
 	}
 
+	if sortBy == "asc" {
+		sort.SliceStable(logs, func(i, j int) bool { return comparator(logs[i], logs[j]) })
+	} else {
+		sort.SliceStable(logs, func(i, j int) bool { return comparator(logs[j], logs[i]) })
+
+	}
+
 	return nil
+
 }
 
 func NewLogSorter(log logger.Logger) LogSorter {
